c5555f3a26fe777e7b2a66d9f20cf362de71993d
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-18 18:57:51 +0530

    add type casting

---
+++ 01_java_fundamentals/01_variables_and_data_types/type_casting.java
@@ -0,0 +1,14 @@
+public class type_casting {
+    public static void main(String[] args) {
+        float a = 9.83f ;
+
+        int b = (int) a ;
+
+        System.out.println(b);
+
+        char ch = 'a';
+        int x = (int) ch ;
+        System.out.println(x);
+    }
+    
+}

b5a89990ca980498b2c1ecd9b8c619b4359be124
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-20 17:34:39 +0530

    add : ternary operator

---
+++ 01_java_fundamentals/02_conditional_statement/ternary_operator.java
@@ -0,0 +1,8 @@
+public class ternary_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = (a > b) ? a : b; // Ternary operator
+        System.out.println("The greater value is: " + c);
+    }
+}

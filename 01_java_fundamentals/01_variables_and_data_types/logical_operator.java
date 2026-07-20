4a4f00a1595f1ce40c2d38aa189934729d285732
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-20 17:18:17 +0530

    add logial operator

---
+++ 01_java_fundamentals/01_variables_and_data_types/logical_operator.java
@@ -0,0 +1,19 @@
+public class logical_operator {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+
+        // Logical AND operator
+        System.out.println("a && b: " + (a && b));
+
+        // Logical OR operator
+        System.out.println("a || b: " + (a || b));
+
+        // Logical NOT operator
+        System.out.println("!a: " + (!a));
+    }
+    
+}

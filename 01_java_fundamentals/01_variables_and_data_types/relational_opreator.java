6ba6d1e178ce72712871a53b5d375b75dbd123d9
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-20 17:16:47 +0530

    add relational operator

---
+++ 01_java_fundamentals/01_variables_and_data_types/relational_opreator.java
@@ -0,0 +1,18 @@
+public class relational_opreator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+
+        // Relational operators
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+    }
+    
+}

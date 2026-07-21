4afe121c2ae7e17a4aca383eec46636004f0464d
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-21 10:40:48 +0530

    add jdo while loop

---
+++ 01_java_fundamentals/03_loops/do_while_loop.java
@@ -0,0 +1,11 @@
+
+public class do_while_loop {
+    public static void main(String[] args) {
+        int count = 1;
+
+        do {
+            System.out.println("Count: " + count);
+            count++;
+        } while (count <= 5);
+    }
+}

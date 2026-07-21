06432d8aba03bb7bdf8c1d3f15b62dd6bc6c19c8
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-21 10:39:28 +0530

    add pronblem

---
+++ 01_java_fundamentals/03_loops/reverse_number.java
@@ -0,0 +1,15 @@
+
+public class reverse_number {
+    public static void main(String[] args) {
+        int number = 12345;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}

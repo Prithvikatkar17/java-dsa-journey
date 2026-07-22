4d7b3ffb5de2a095d27d7c770a80b971abe7b1b3
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-22 09:15:06 +0530

    add funtion syntax

---
+++ 02_functions/syntax.java
@@ -0,0 +1,17 @@
+
+
+public class syntax {
+
+    public static void func() {
+        System.out.println("Hello World");
+        
+    }
+    public static int add (int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        func();
+        System.out.println(add(5, 10));
+    }
+    
+}

389ab3cecd0c74177226123a8a14853321b14d37
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-22 11:45:12 +0530

    add array creation

---
+++ 03_Arrays/array.java
@@ -0,0 +1,13 @@
+
+
+public class array {
+    public static void main(String[] args) {
+        // creating an array 
+        int arr[] = new int[5];
+
+        int arr2[] = {1, 2, 3, 4, 5};
+
+        String arr3[] = {"Hello", "World"};
+        
+    }
+}

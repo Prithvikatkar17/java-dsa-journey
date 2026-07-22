1a06b22d608e3ed2ff9f5cee742e5812cc3d8578
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-07-22 15:56:41 +0530

    add array creation

--- 03_Arrays/array.java
+++ 03_Arrays/array.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
@@ -7,7 +7,20 @@ public class array {
 
         int arr2[] = {1, 2, 3, 4, 5};
 
-        String arr3[] = {"Hello", "World"};
-        
+        String arr4[] = {"Hello", "World"};
+
+
+        // inputting values in an array
+        int arr3[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers: ");
+
+        arr3[0] = sc.nextInt();
+        arr3[1] = sc.nextInt();
+        arr3[2] = sc.nextInt();
+        arr3[3] = sc.nextInt();
+        arr3[4] = sc.nextInt();
+
+        System.out.println("The numbers " + arr3[0] + " " + arr3[1] + " " + arr3[2] + " " + arr3[3] + " " + arr3[4]);
     }
 }

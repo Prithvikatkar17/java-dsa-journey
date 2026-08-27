5a9f6a57e992ede4421f29df4ce6466f3cb8c57f
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-08-27 11:45:56 +0530

    add Array list

---
+++ 09_ArrayList/syntax.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class syntax {
+    public static void main(String[] args) {
+        // Create an ArrayList of integers
+        ArrayList<Integer> numbers = new ArrayList<>();
+        // Add elements to the ArrayList
+        numbers.add(10);
+        numbers.add(1, 11) ;
+
+        //get the element at index 1
+        int element = numbers.get(1);
+        System.out.println(element);
+
+        // Remove the element at index 0
+        numbers.remove(0);
+        System.out.println(numbers);
+
+        // set the element at index 0 to 20
+        numbers.set(0, 20);
+        System.out.println(numbers);
+
+        // check if the ArrayList contains the element 20
+        boolean contains20 = numbers.contains(20);
+        System.out.println(contains20);
+
+        //size of the ArrayList
+        int size = numbers.size();
+        System.out.println(size);
+
+    }
+    
+}

c037cfc89022a1055a7cfb396a6143877c0adf6a
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-09-02 17:41:10 +0530

    add addfirst methods

---
+++ 12_linked_list/linked_list.java
@@ -0,0 +1,27 @@
+
+
+public class linked_list {
+    public class Node {
+        int data ;
+        Node next ;
+        public Node(int data){
+            this.data = data ;
+            this.next = null ;
+        }
+    }
+    public Node head ;
+    public Node tail ;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data) ;
+        if(head == null){
+            head = tail = newNode ;
+            return ;
+        }
+        newNode.next = head ;
+        head = newNode ;
+    }
+    public static void main(String[] args) {
+        
+    }
+}

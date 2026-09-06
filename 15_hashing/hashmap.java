8c14911386ee8726855695af7bbdd2a008b7f3d0
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-09-06 11:15:57 +0530

    add hash methods

--- 15_hashing/hashmap.java
+++ 15_hashing/hashmap.java
@@ -6,6 +6,23 @@ public class hashmap {
         map.put("banana", 10);
         System.out.println(map.get("apple"));
         System.out.println(map.get("banana"));
+
+
+         HashMap<String, Integer> map1 = new HashMap<>();
+        map1.put("apple", 5);
+        map1.put("banana", 10);
+        System.out.println(map1.get("apple"));
+        System.out.println(map1.get("banana"));
+
+        map1.remove("orange");
+        System.out.println(map1.containsKey("orange"));
+        System.out.println(map1.containsValue(15));
+
+        System.out.println(map1.size());
+        System.out.println(map1.isEmpty()); 
+
+       
+
     }
     // hashmap methods
     // put(K key, V value)
@@ -16,5 +33,9 @@ public class hashmap {
     // size()
     // isEmpty()
     // clear()
+
+    // example of hashmap methods
+   
+
      
 }

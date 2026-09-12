9bc47038239545b72bbee3bcc7ef1c1fd204c794
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-09-12 14:58:33 +0530

    add inbuild likedl list

---
+++ 12_linked_list/inbuild_LL.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class inbuild_LL {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList<>() ;
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(3);
+
+        System.out.println(ll);
+    }
+}
